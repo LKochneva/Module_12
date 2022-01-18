af37d9063112c62fcef3487423e5cef93f6e016e
LKochneva <kochneva.liliya@gmail.com>
2022-01-18 14:27:08 +0200

    added new file

--- main.py
+++ main.py
@@ -2,7 +2,9 @@ value = int(input('Введите сумму вклада: '))
 per_cent = {'ТКБ': 5.6, 'СКБ': 5.9, 'ВТБ': 4.28, 'СБЕР': 4.0}
 deposit = []
 for key in per_cent:
-    deposit.append(per_cent[key] / 100 * value)
+    deposit.append(int(per_cent[key] / 100 * value))
 maximum = max(deposit)
+deposit.sort()
+deposit.reverse()
 print(deposit)
 print("Максимальная сумма — " + str(maximum))
